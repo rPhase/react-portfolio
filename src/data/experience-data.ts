355e1a44d0c8052ea719e5408db7ab06ef290b25
rPhase <25046850+rPhase@users.noreply.github.com>
2024-10-22 23:19:20 -0700

    prettier settings

--- src/data/experience-data.ts
+++ src/data/experience-data.ts
@@ -12,29 +12,29 @@ export interface IExperience {
 
 export const experience: IExperience[] = [
   {
-    name: "illum ex dolor",
-    title: "Developer",
-    url: "www.example.com",
-    imgSrc: "experience-1",
-    startDate: "June 2020",
+    name: 'illum ex dolor',
+    title: 'Developer',
+    url: 'www.example.com',
+    imgSrc: 'experience-1',
+    startDate: 'June 2020',
     description:
-      "Id et facilis enim. Libero hic animi. Repudiandae commodi ut soluta. Et quam quidem ipsum libero maxime vel. Ut quam quasi qui. Natus natus quis recusandae.",
+      'Id et facilis enim. Libero hic animi. Repudiandae commodi ut soluta. Et quam quidem ipsum libero maxime vel. Ut quam quasi qui. Natus natus quis recusandae.',
     bulletPoints: [
-      "Ut vero et iure.",
-      "Sit alias voluptatum reprehenderit nulla non.",
-      "Quidem eos et.",
+      'Ut vero et iure.',
+      'Sit alias voluptatum reprehenderit nulla non.',
+      'Quidem eos et.',
     ],
-    techStack: ["JavaScript", "TypeScript", "NodeJS", "ReactJS"],
+    techStack: ['JavaScript', 'TypeScript', 'NodeJS', 'ReactJS'],
   },
   {
-    name: "nisi ea autem",
-    title: "Developer",
-    url: "www.example.com",
-    imgSrc: "experience-1",
-    startDate: "2018",
-    endDate: "April 2020",
+    name: 'nisi ea autem',
+    title: 'Developer',
+    url: 'www.example.com',
+    imgSrc: 'experience-1',
+    startDate: '2018',
+    endDate: 'April 2020',
     description:
-      "Sed magnam nisi libero cupiditate ab vel possimus. Quia eaque velit rerum dolorum. Hic doloribus sed. ",
-    techStack: ["Python", "C++", "Java"],
+      'Sed magnam nisi libero cupiditate ab vel possimus. Quia eaque velit rerum dolorum. Hic doloribus sed. ',
+    techStack: ['Python', 'C++', 'Java'],
   },
 ];
